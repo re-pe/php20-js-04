f29e4073d3a0a11400d9788521288fef989ed6f1
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-01-13 13:46:55 +0200

    Add Keyboard events

--- 04-2/js/main.js
+++ 04-2/js/main.js
@@ -1,40 +1,51 @@
 (() => {
-  const numVal = document.querySelector('#numVal');
-  const okBtn = document.querySelector('#okBtn');
-  const progressBar = document.querySelector('#progressBar');
-  const classNames = ['bg-danger', 'bg-info', 'bg-success'];
-  const classRegExp = /\bbg-.+?\b/g;
-  
-  const getProgressClass = () => {
-    const val = Number(numVal.value);
-    if (val < 40) {
-      return classNames[0];
-    }
-    if (val < 70) {
-      return classNames[1];
-    }
-    return classNames[2];
-  };
+    const numVal = document.querySelector('#numVal');
+    const okBtn = document.querySelector('#okBtn');
+    const progressBar = document.querySelector('#progressBar');
+    const classNames = ['bg-danger', 'bg-info', 'bg-success'];
+    const classRegExp = /\bbg-.+?\b/g;
 
-  const setProgressBarWidth = () => {
-    progressBar.style.width = `${numVal.value}%`;
-    progressBar.setAttribute('aria-valuenow', `${numVal.value}`);
-  }
+    const getProgressClass = () => {
+        const val = Number(numVal.value);
+        if (val < 40) {
+            return classNames[0];
+        }
+        if (val < 70) {
+            return classNames[1];
+        }
+        return classNames[2];
+    };
 
-  numVal.addEventListener('input', (event) => {
-    if (event.target.value < 0) {
-      event.target.value = 0;
+    const setProgressBarWidth = () => {
+        progressBar.style.width = `${numVal.value}%`;
+        progressBar.setAttribute('aria-valuenow', `${numVal.value}`);
     }
-    if (event.target.value > 100) {
-      event.target.value = 100;
-    }
-  })
 
-  okBtn.addEventListener('click', (event) => {
-    const barClass = getProgressClass();
-    progressBar.className = progressBar.className.replace(classRegExp, '');
-    progressBar.classList.toggle(barClass);
-    setProgressBarWidth();
-  })
+    const handleEvents = () => {
+        const barClass = getProgressClass();
+        progressBar.className = progressBar.className.replace(classRegExp, '');
+        progressBar.classList.toggle(barClass);
+        setProgressBarWidth();
+    }
+
+    numVal.addEventListener('input', (event) => {
+        if (event.target.value < 0) {
+            event.target.value = 0;
+        }
+        if (event.target.value > 100) {
+            event.target.value = 100;
+        }
+    })
+
+    numVal.addEventListener('keypress', (event) => {
+        if (!["Enter", "NumpadEnter"].includes(event.code)) {
+            return;
+        }
+        handleEvents();
+    })
+
+    okBtn.addEventListener('click', (event) => {
+        handleEvents();
+    })
 })();
 
